fe846329226fd4af68d14faa6adf456165c16e40
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-06-13 15:21:17 +0100

    modifying strings

---
+++ Strings/slicing.py
@@ -0,0 +1,17 @@
+"""
+Slicing of string is needed for getting substrings of a string
+"""
+name = "Yartomizanoni"
+print(name[3:10])
+print(name[-10:-3])
+
+
+#to slice from the start to a point
+print(name[:10])
+
+#slicing from a point to the end
+print(name[9:])
+
+
+#slicing from the end using negative numbers
+print(name[-6:-1])
