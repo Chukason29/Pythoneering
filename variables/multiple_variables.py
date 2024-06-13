3d042a10d418cd899ae6c1bf0eee2be2a2c8e778
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-06-13 11:02:33 +0100

    multiple value variables

---
+++ variables/multiple_variables.py
@@ -0,0 +1,17 @@
+#variable assignment can be done in a single line
+
+a, b, c = "first", "second", "third"
+print(a)
+print(b)
+print(c)
+
+# If multiple variables, have same values then do below
+
+x = y = z = "Trinity"
+print(x)
+print(y)
+print(z)
+
+#You can use UNPACKING to assign variables too
+var1, var2, var3 = ["Emeka", "Tommyzan", "Wisdom"]
+print(var1, var2, var3)
