f6a746469da8cb9b90cd9923bdd5fbbb8b1f1d31
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-06-13 10:40:58 +0100

    casting variables

--- variables/Casting.py
+++ variables/Casting.py
@@ -1,3 +1,14 @@
-import sys
+"""
+Casting is a method of forcing a particular data type on a variable
+"""
 
-print(sys.version)
+a = '5'
+
+print(type(int(a))) # a is normally a string but because we casted it to an integer this will print out a string
+print(int(a) + 7) # will give you 12
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(f"{x}, {y}, {z}")
--- variables/variables.py
+++ variables/variables.py
@@ -4,4 +4,5 @@ Varibale is declared at the point of assignment
 a = 5
 x = 2
 a = 4 # here the value of a has changed from 5 to 4
+a = "Ebuka" #here a is now a string
 print(a + x)
