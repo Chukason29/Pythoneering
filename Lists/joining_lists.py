013db0e91ffa03095fed675d2f43d9c74541fe21
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-06-21 11:22:29 +0100

    joining list

---
+++ Lists/joining_lists.py
@@ -0,0 +1,9 @@
+"""
+list can be joined using concatenation
+"""
+
+fruits1 = ["Apple", "Mango", "Orange", "Apricot", "Guavas"]
+fruits2 = ["Cherry", "Watermelon", "Pineapple", "Apricot"]
+
+fruits3 = fruits1 + fruits2
+print(fruits3) #fruit3 is the combination of
