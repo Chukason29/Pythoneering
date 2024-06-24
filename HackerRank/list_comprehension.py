2fe7826dcb78d471fe335c79155a21af27dc8a52
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-06-24 19:50:33 +0100

    list comprehension hackerrank

---
+++ HackerRank/list_comprehension.py
@@ -0,0 +1,26 @@
+"""
+Let's learn about list comprehensions! You are given three integers  
+and  representing the dimensions of a cuboid along with an integer . 
+Print a list of all possible coordinates given by  on a 3D grid where the sum of  
+is not equal to . Here, . Please use list comprehensions rather than multiple loops, as a learning exercise.
+
+All permutations of  are:
+
+Print an array of the elements that do not sum to .
+"""
+
+newRange = [ i for i in range(1,100)  if i%2 ==0]
+
+#x = int(input())
+#y = int(input())
+#z = int(input())
+#n = int(input())*/
+x = 1
+y = 1
+z = 2
+n = 3
+
+permutations = [[a, b, c] for a in range(x+1) for b in range(y+1) for c in range(z+1)]
+print(permutations)
+total = [i for i in permutations if sum(i) != n]
+print(total)
---
+++ Lists/sum_list.py
@@ -0,0 +1,4 @@
+# To add all values in a list  use sum
+
+numbers = [21, 3, 5, 6, 10, 16]
+print(sum(numbers))
