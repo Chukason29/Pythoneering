832a098a03075e6908fd6748c99ae02ce4dd12c9
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-01 12:38:57 +0100

    tuple meaning

--- Tuple/Access_tuple.py
+++ Tuple/Access_tuple.py
@@ -1,3 +1,6 @@
 """
+Tuples are like list just that you can mutate a tuple after declaration
+Tuples are ordered==> Meaning they indexed the position of each item doesn't change with time
+Tuples are immutable ==>
 
 """
