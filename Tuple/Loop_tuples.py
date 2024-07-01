ad4bc623b516935c0ef980dbc6f59028ce11462d
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-01 13:34:16 +0100

    looping a tuple using while

--- Tuple/Loop_tuples.py
+++ Tuple/Loop_tuples.py
@@ -3,9 +3,20 @@ Like List, Tuples can be looped through using for in, for or while loops
 """
 Continents = ("Africa", "Europe", "Asia", "South America", "North America", "Oceania", "Antartica")
 
+#Using forLoop
 search = "e"
 for i in Continents:
     if "e" in i:
         print(f"{i}, contains {search}")
     else:
         print(f"{i}, does not conatin {search}")
+
+
+#Using While Loop
+count = 0
+while count < len(Continents):
+    if "e" in i:
+        print(f"{i}, contains {search}")
+    else:
+        print(f"{i}, does not conatin {search}")
+    count = count + 1
