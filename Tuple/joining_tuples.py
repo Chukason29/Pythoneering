3b39f20aa8cd7437b3913c7c8c2d1c671d392072
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-01 13:44:11 +0100

    joining tuples

---
+++ Tuple/joining_tuples.py
@@ -0,0 +1,9 @@
+"""
+Like lists, two or more tuples can be joined together
+"""
+yellowFruits = ("Mango", "Banana", "Sweet WaterMelon")
+greenFruits = ("Apple", "Guava", "Sour Sop")
+
+allFruits = yellowFruits + greenFruits
+
+print(allFruits)
