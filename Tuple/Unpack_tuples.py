c503f746ab4de496839dba380cb1826115fa665d
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-01 13:18:38 +0100

    Unpacking Tuples

---
+++ Tuple/Unpack_tuples.py
@@ -0,0 +1,12 @@
+"""
+Unpacking tuples is needed is a concept where tuple items are assigned to variales
+"""
+Continents = ("Africa", "Europe", "Asia", "South America", "North America", "Oceania", "Antartica")
+
+#let's unpack the tuples above 
+
+(Nigeria, Germany, China, Brazil, Mexico, Australia, SnowLine) = Continents
+
+print(Nigeria) # will print Africa
+print(Germany) #will print Europe
+print(China) #will print Asia
