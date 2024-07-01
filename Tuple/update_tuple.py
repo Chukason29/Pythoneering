67ff79b4c3cdbd15567ad41317b258d0cdbeb2a1
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-01 13:06:56 +0100

    updating tuples

---
+++ Tuple/update_tuple.py
@@ -0,0 +1,15 @@
+"""
+Although we said before that we cannot mutate a tuple.
+To do so, you must first convert to a list, then to mutate an reconvert to  TUPLE
+"""
+
+fruits = ("Apple", "Mango", "Orange", "Apricot", "Guavas", "Cherry", "Watermelon", "Pineapple", "Apricot", "3Maple")
+
+#Let's remove first and last item from the tuple above
+fruitsList = list(fruits)
+fruitsList.remove(fruitsList[0])
+fruitsList.remove(fruitsList[-1])
+
+fruitsList = tuple(fruitsList)
+
+print(type(fruitsList))
