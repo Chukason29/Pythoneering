121b07dc3e49f134d9f06701c3194a75d7caf59e
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-01 13:58:03 +0100

    count() method

---
+++ Tuple/Tuple_methods.py
@@ -0,0 +1,9 @@
+"""
+Let's See some methods of tuples
+"""
+Continents = ("Africa", "Europe", "Asia", "South America", "North America", "Oceania", "Antartica")
+ContinentsList = ["Africa", "Europe", "Asia", "South America", "North America", "Oceania", "Antartica"]
+
+#.count() tells you the number of items an item occurs in a tuple
+print(Continents.count("Africa"))
+print(Continents.count("Asia"))
