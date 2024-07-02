d700038eec12923e5901872e78fd3df11c980cb3
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-02 15:41:36 +0100

    set loopings

---
+++ Sets/Loop_sets.py
@@ -0,0 +1,8 @@
+"""
+Set items can only be accessed using loops
+"""
+
+fruits = {"Apple", "Mango", "Orange", "Apricot", "Guavas", "Cherry", "Watermelon", "Pineapple", "Apricot", "3Maple"}
+
+for x in fruits:
+    print(x, end=", ")
