8537baa4c749fa40907b43330153e2097531c2a0
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-03 14:17:50 +0100

    get dict keys and values

--- Dictionaries/Accessing_dict.py
+++ Dictionaries/Accessing_dict.py
@@ -11,3 +11,12 @@ student = {
 print(student["name"])
 # will gie you same result as 
 print(student.get("name"))
+
+
+#.keys() gives you all the keys of the dict in a list
+dictKeys = student.keys()
+print(dictKeys)
+
+#.values() gives you all the values of the dict in a list
+dictValues = student.values()
+print(dictValues)
