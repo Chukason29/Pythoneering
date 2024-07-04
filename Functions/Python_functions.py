07a3e8d7ca8247ad6b6018c8194baf5acd966cc4
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-04 12:34:50 +0100

    Python Functions

---
+++ Functions/Python_functions.py
@@ -0,0 +1,12 @@
+"""
+Python functions are block of code for a particular process
+"""
+# Syntax
+
+#Functions Without parameters
+def addNums():
+    a = 5
+    b = 7
+    print( a + b)
+
+addNums() #This is a function Call
