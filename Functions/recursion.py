556690affa3027dd50b13a910f6fdd0fd09d2801
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-06 15:22:05 +0100

    Correct Fibonacci Series

--- Functions/recursion.py
+++ Functions/recursion.py
@@ -2,15 +2,6 @@
 Recursion always starts from the end to handle the answer
 """
 #Factorial
-def fact(n):
-    if n > 1: #base case
-        factorial = n * fact(n-1)
-    else:
-        factorial = 1
-    return factorial
-
-print(fact(5))
-
 def factorial(n):
     if n == 0:
         return 0
@@ -20,10 +11,14 @@ def factorial(n):
         return n * factorial(n - 1)
 print(factorial(5))
 
+
+# Fibonaccci Series
 def fibonnacci(n):
-    if n > 1:
-        fibnum = fibonnacci(n-1) +fibonnacci(n + 1)
+    if n == 1:
+        return 1
+    elif n == 0:
+        return 0    
     else:
-        n = 1
-    return fibnum
+        return fibonnacci(n-1) +fibonnacci(n-2)
+print(fibonnacci(6))
 
