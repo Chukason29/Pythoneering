78c1a18528b97f18c8d9d766cc6a720b89b90be2
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-06 15:40:09 +0100

    summation of numbers in a list

--- Functions/recursion.py
+++ Functions/recursion.py
@@ -22,3 +22,20 @@ def fibonnacci(n):
         return fibonnacci(n-1) +fibonnacci(n-2)
 print(fibonnacci(6))
 
+
+
+# Sum of all numbers in the list
+def sumList (numList):
+    if len(numList) == 0: #checks if the list is empty
+        return 0
+    else: # If list is not empty then
+        return numList[0] + sumList(numList[1:]) #Add the first number to the remaining numbers
+print(sumList([1, 2, 3, 4, 5]))
+
+def reverseString(theString):
+    if len(theString) == 0:
+        return theString
+    else:
+        return theString[-1] + reverseString(theString[:-1])
+
+print(reverseString("Arsenal"))
