336737e8edc4f77f66de597cbd0b8beebbc7e2c9
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-06 16:22:28 +0100

    Lambda with map function

--- Functions/lambda_functions.py
+++ Functions/lambda_functions.py
@@ -1,3 +1,4 @@
+import math
 """
 Lambda functions are short functions stored to a variable
 
@@ -10,7 +11,12 @@ print(add(3, 5)) #print 8
 
 
 #Write a lambda function that takes two arguments, a and b, and returns their product. Use this lambda function to calculate the product of 4 and 5.
-
 product = lambda a, b: a * b
-
 print(product(4, 5))
+
+#Given the list numbers = [1, 2, 3, 4, 5], use the map() function with a lambda to create a new list where each number is squared. Print the new list.
+def square(n):
+    return n**2
+
+newList = lambda myList: list(map(square, myList))
+print(newList([1, 2, 3, 4, 5]))
