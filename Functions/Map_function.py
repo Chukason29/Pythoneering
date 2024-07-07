cbab7866871e9277b4ac1de6a55005adea8ff8d1
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-07 18:54:33 +0100

    Map function squared

--- Functions/Map_function.py
+++ Functions/Map_function.py
@@ -2,5 +2,8 @@
 The map function is used to carry out an operation on every item on the list
 """
 #Square of Numbers: Given a list of integers, return a list where each integer is squared.
-def squared(n):
-    return n**2
+
+squared = lambda n : n**2
+myList = [2, 4, 6, 8, 1, 12]
+squaredList = list(map(squared, myList))
+print(squaredList)
