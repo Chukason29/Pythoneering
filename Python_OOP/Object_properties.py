e215834686a74769c9c62fdb8e33739fd47e1e6f
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-08 14:45:18 +0100

    product data class

--- Python_OOP/Object_properties.py
+++ Python_OOP/Object_properties.py
@@ -16,9 +16,33 @@ myCountry.name = "South Africa"
 print(myCountry.name) #prints South Africa
 
 
-#get 20% off all the products in the database
+productData = {
+    "item1":{
+        "name":"Phone",
+        "price": 3000,
+        "category": "Electronics"
+    },
+    "item2":{
+        "name":"Shoe",
+        "price": 500,
+        "category": "fashion"
+    },
+    "item3":{
+        "name":"Toy Car",
+        "price": 1000,
+        "category": "Kiddies"
+    }
+}
+#get 20% off all the products in the database using OOP
 class Product:
     def __init__(self, name, price, category):
         self.name = name
         self.price = price
         self.category = category
+    def twentyPercentOff(self, price): #removes 20% from each item's price
+        return price * 0.2
+    
+    def allproduct(self, db):
+        for item in db:
+            self.twentyPercentOff(item["price"])
+    
