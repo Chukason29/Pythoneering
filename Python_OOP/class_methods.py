9532ffdf7929674d4bac7469bab096804267aca0
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-08 12:17:01 +0100

    object method one

---
+++ Python_OOP/class_methods.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self, name, model, colour) :
+        self.name = name
+        self.model = model
+        self.colour = colour
+    
+    def ChangeColor(this):
+        this.colour = "Red"
+        return this.colour
+
+
+myCar = Car("Benz", "A54", "White")
+print(myCar.ChangeColor()) #Prints Red in
