56e002670637b0e353152bfdd26028401c8829f0
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-08 11:40:09 +0100

    creating objects from a class

--- Python_OOP/classes.py
+++ Python_OOP/classes.py
@@ -11,3 +11,8 @@ class Student:
     def __init__(self, name, age): # This is a constructor function, it is used to a
         self.name = name
         self.age = age
+
+#creating sn object
+newStudent = Student("Tommyzan", 40) #The arguments reps the name and age in the __init__() of the student class
+
+print(f"My name is  {newStudent.name} and I am {newStudent.age} years old")
