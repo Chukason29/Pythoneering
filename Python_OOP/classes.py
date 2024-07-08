96dfc14cfc877bc58f2582bb9d004574c1695696
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-08 11:25:36 +0100

    class intro

---
+++ Python_OOP/classes.py
@@ -0,0 +1,13 @@
+"""
+A class is the blueprint used in creating objects
+A class has basically three parts
+
+==> Constructor
+==> Properties
+==> Methods
+"""
+
+class Student:
+    def __init__(self, name, age): # This is a constructor function, it is used to a
+        self.name = name
+        self.age = age
