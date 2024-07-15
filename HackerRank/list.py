c2730a0ec35a445b1e3f37001ba5269eaca52d4c
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-15 18:00:42 +0100

    problem solved

--- HackerRank/list.py
+++ HackerRank/list.py
@@ -5,18 +5,17 @@ if __name__ == '__main__':
         entry = input()
         data = entry.split()
         if(data[0] == "insert"):
-            myList.insert(data[1], data[2])
+            myList.insert(int(data[1]), int(data[2]))
         elif(data[0] == "print"):
             print(myList)
         elif (data[0] == "remove"):
-            myList.remove(data[1])
+            myList.remove(int(data[1]))
         elif (data[0] == "append"):
-            myList.append(data[1])
+            myList.append(int(data[1]))
         elif (data[0] == "sort"):
             myList.sort()
         elif (data[0] == "pop"):
             myList.pop()
         elif (data[0] == "reverse"):
             myList.reverse()
-    
-print(data)
+print(myList)
