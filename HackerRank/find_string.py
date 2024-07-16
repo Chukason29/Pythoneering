732c895da91a7a1ab6752e3c57ef6fc8d22190c8
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-16 13:11:51 +0100

    final length

--- HackerRank/find_string.py
+++ HackerRank/find_string.py
@@ -6,9 +6,15 @@ def count_substring(string, sub_string):
     count = 0
     if sub_string_length == 1:
         for i in range(0, len(string)):
-            if sub_string == i:
+            if sub_string == string[i]:
                 count = count + 1
-    return count
+    else:
+        for i in range(0, len(string)):
+            if len(string[i:]) == sub_string_length:
+                if string[i: ] == sub_string:
+                    count = count + 1
+                    return count
+                
 
 if __name__ == '__main__':
     string = input().strip()
