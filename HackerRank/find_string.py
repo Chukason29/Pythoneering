3c817c1c7ac8e8fbfcc807ad8f9b47774acef567
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-16 12:49:15 +0100

    find string start

---
+++ HackerRank/find_string.py
@@ -0,0 +1,18 @@
+# length of subtring is  =  1; do a normal looping
+#if s[i:] is equal to the length of substring , end the loop
+
+def count_substring(string, sub_string):
+    sub_string_length = len(sub_string)
+    count = 0
+    if sub_string_length == 1:
+        for i in range(0, len(string)):
+            if sub_string == i:
+                count = count + 1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
