291faab782b201f9751b6acb30c20f50413c7c89
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-17 13:39:16 +0100

    static methods

--- python_OOP_Practice/class_definition.py
+++ python_OOP_Practice/class_definition.py
@@ -1,12 +1,19 @@
 class Student:
-    #__init__ is used to assign  properties to objects created from this class
-    def __init__(self, name: str, age: int, height: float): #always assign 
+    #TODO Add class variables
+
+    #__init__ is used to assign  properties to objects created from this class or create instance variables
+    def __init__(self, name: str, age: int, height: float): #always assign types to properties
         self.name = name
         self.age = age
         self.height = height
+    
+    #TODO write out static methods that belong only to classes
+    #@staticmethod
 
 #Create objects from Students class
 
 student1 = Student("Uche", 1.4, 6)
-print(student1)
+print(student1.name)
+print(student1.age)
+print(student1.height)
     
