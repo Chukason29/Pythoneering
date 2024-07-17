1dbd80c1b0f22dc725f89c25ae596948d7393e2c
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-17 13:54:50 +0100

    static and instance variables

--- python_OOP_Practice/class_definition.py
+++ python_OOP_Practice/class_definition.py
@@ -9,11 +9,17 @@ class Student:
     
     #TODO write out static methods that belong only to classes
     #@staticmethod
+    def staticMethod(params): #static methods do not require a self parameter 
+        pass
+
+
+    #TODO write out instance methods accessible by object(instance)
+    #because instance methods are accessible from an object hence, self is needed
+    def instanceMethod (self, params):
+        pass
+
 
 #Create objects from Students class
-
 student1 = Student("Uche", 1.4, 6)
-print(student1.name)
-print(student1.age)
-print(student1.height)
+
     
