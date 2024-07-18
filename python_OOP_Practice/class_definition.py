a91447c091b5a5111c833ec5f95cacf57981127a
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-18 12:31:23 +0100

    instance created

--- python_OOP_Practice/class_definition.py
+++ python_OOP_Practice/class_definition.py
@@ -2,15 +2,18 @@ class Student:
     #TODO Add class variables
     admin = "Chukwuebuka"
 
+    #This list is used to store all instances as they are created
+    allInstance = []
     #__init__ is used to assign  properties to objects created from this class or create instance variables
     def __init__(self, name: str, age: int, height: float): #always assign types to properties
+        #TODO assert keyword is used to control the kind or range of data e.g no -ve values
+        #assert ca1 >=0 and ca1 <=20, f"CA1 shouldn't be more than 20 nor less than 0"
+        #assert ca2 >=0 and ca2 <=20,  f"CA2 shouldn't be more than 20 nor less than 0"
         self.name = name
         self.age = age
         self.height = height
-    
-    #TODO assert keyword is used to control the kind or range of data
-    #assert ca1 >=0 and ca1 <=20, f"CA1 shouldn't be more than 20 nor less than 0"
-    #assert ca2 >=0 and ca2 <=20,  f"CA2 shouldn't be more than 20 nor less than 0" 
+
+        Student.allInstance.append(self) #with this all instances/objects are appended to the list at the time of creation
     
     #TODO write out static methods that belong only to classes
     #@staticmethod
