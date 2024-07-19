32425e925245938755feed2a7bf6dfd6a2164ddf
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-19 12:57:08 +0100

    finished class methods

--- python_OOP_Practice/class_methods.py
+++ python_OOP_Practice/class_methods.py
@@ -8,26 +8,24 @@ class Student:
         self.name = name
         self.age = age
         self.height = height
+        Student.all_instances.append(self) #appends all instances to a "all_instances" list
 
-        Student.all_instances.append(self) #appends all instances to a list
-
-    #TODO using class methods to automatically instantiate objects from a database
-    @classmethod #==> always use decorators
-    def instantiate_objects(cls): #what self is to instance methods is what cls is to class methods
-        #reading data from a csv file and converting to a list
+    @classmethod
+    #This method creates objects
+    def current_object(cls): 
         with open('C:/Users/USER/Desktop/Pythoneering/python_OOP_Practice/data.csv', 'r') as f:
             reader = csv.DictReader(f)
             students = list(reader)
-        
-        for student in students:
-            #creating an instance for each record on the
-            Student(
-                name = student.get("name"),
-                age = student.get("age"),
-                height = student.get("height")
-            )
+            for student in students:
+                if int(student["age"]) <= 20:
+                    Student(
+                        name = student["name"],
+                        age = int(student["age"]),
+                        height = float(student["height"])
+                    )
     def __repr__(self):
         return f"Student({self.name}, {self.age}, {self.height})"
         pass
-Student.instantiate_objects()
+#Student.instantiate_objects()
+Student.current_object()
 print(Student.all_instances)
